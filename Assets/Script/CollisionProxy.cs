c0ea62aed2d3d33ffe3c71be81fa487223e72b70
justin-prat <justinprat07@gmail.com>
2025-05-15 11:57:11 +0200

    Added proxy

--- Assets/Script/CollisionProxy.cs
+++ Assets/Script/CollisionProxy.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
-public class CollisionProxy : MonoBehaviour
+public class CollisionProxy : MonoBehaviour, IHealth
 {
-    
+    [SerializeField] private EntityBrain brain;
+
+    public float Health => brain.Health;
+
+    public float MaxHealth => brain.MaxHealth;
+
+    public void Heal(float amount)
+    {
+        brain.Heal(amount);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        brain.TakeDamage(amount);
+    }
 }
--- Assets/Script/Player/PlayerBrain.cs
+++ Assets/Script/Player/PlayerBrain.cs
@@ -1,22 +1,5 @@
 using UnityEngine;
 
-public class PlayerBrain : MonoBehaviour, IHealth
+public class PlayerBrain : EntityBrain
 {
-    [SerializeField] private Updater _updater;
-
-    private float health;
-    private float maxHealth;
-
-    public float Health { get { return health; } }
-    public float MaxHealth { get { return maxHealth; } }
-
-    public void Heal(float amount)
-    {
-        health = Mathf.Clamp(health + amount, 0, maxHealth);
-    }
-
-    public void TakeDamage(float amount)
-    {
-        health = Mathf.Clamp(health - amount, 0, maxHealth);
-    }
 }
